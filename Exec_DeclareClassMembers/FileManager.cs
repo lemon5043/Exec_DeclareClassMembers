5fad1a4e4a4354477a3ce8409d6ab275e6395bf9
伯翰 侯 <john860703@gmail.com>
2022-10-24 16:43:51 +0800

    加入專案檔案。

---
+++ Exec_DeclareClassMembers/FileManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_DeclareClassMembers
+{
+	internal class FileManager
+	{
+		public void Save(){}
+	}
+}
---
+++ Exec_DeclareClassMembers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_DeclareClassMembers
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			FileManager FM = new FileManager();
+		}
+	}
+}
